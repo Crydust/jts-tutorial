c8bea02d59253410b077fd4a7b43b658bd7b41d3
Kristof Neirynck <Crydust@users.noreply.github.com>
2026-04-22 23:38:00 +0200

    Add rendering hints to graphics

--- src/main/java/com/smartycoder/ui/JTSVisualisationPanel.java
+++ src/main/java/com/smartycoder/ui/JTSVisualisationPanel.java
@@ -2,8 +2,11 @@ package com.smartycoder.ui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JPanel;
 /**
@@ -24,7 +27,21 @@ public class JTSVisualisationPanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
- 
+
+		if (g instanceof Graphics2D g2d) {
+			g2d.addRenderingHints(Map.of(
+					RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON,
+					RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY,
+					RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY,
+					RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY,
+					RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC,
+					RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE,
+					RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE,
+					RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON,
+					RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON
+			));
+		}
+
 		g.setColor(Color.darkGray);
 		
 		g.fillRect(0, 0, 600, 600);
