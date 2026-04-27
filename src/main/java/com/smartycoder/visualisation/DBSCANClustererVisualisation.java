208bd6a01a12b2e68a50e652875d44cd339aab75
Kristof Neirynck <Crydust@users.noreply.github.com>
2026-04-27 23:35:16 +0200

    DBSCANClusterer

---
+++ src/main/java/com/smartycoder/visualisation/DBSCANClustererVisualisation.java
@@ -0,0 +1,76 @@
+package com.smartycoder.visualisation;
+
+import com.smartycoder.ui.DrawMultiPoint;
+import com.smartycoder.ui.DrawPolygon;
+import com.smartycoder.ui.DrawingCommand;
+import org.hipparchus.clustering.Cluster;
+import org.hipparchus.clustering.Clusterable;
+import org.hipparchus.clustering.DBSCANClusterer;
+import org.hipparchus.clustering.distance.EuclideanDistance;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.MultiPoint;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.impl.CoordinateArraySequence;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.smartycoder.ui.VisualisationUtil.show;
+
+public class DBSCANClustererVisualisation {
+
+    public static void main(String[] args) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+
+        int pointCount = 400;
+        List<Coordinate> coordinates = new RandomCoordinates(50, 500, 50, 500)
+                .generate(pointCount);
+        Point[] points = coordinatesToPoints(pointCount, coordinates, geometryFactory);
+        MultiPoint multiPoint = geometryFactory.createMultiPoint(points);
+
+        List<Cluster<ClusterableCoordinate>> clusters = new DBSCANClusterer<ClusterableCoordinate>(
+                30, 5)
+                .cluster(coordinates.stream()
+                        .map(ClusterableCoordinate::new)
+                        .toList());
+        List<Polygon> clusterPolygons = new ArrayList<>();
+        for (Cluster<ClusterableCoordinate> cluster : clusters) {
+            Coordinate[] clusterCoordinates = cluster.getPoints().stream()
+                    .map(ClusterableCoordinate::coordinate)
+                    .toArray(Coordinate[]::new);
+            MultiPoint clusterMultiPoint = geometryFactory.createMultiPoint(new CoordinateArraySequence(clusterCoordinates));
+            Polygon clusterPolygon = (Polygon) clusterMultiPoint.convexHull();
+            clusterPolygons.add(clusterPolygon);
+        }
+
+        List<DrawingCommand> drawingCommands = new ArrayList<>();
+        for (Polygon clusterPolygon : clusterPolygons) {
+            drawingCommands.add(new DrawPolygon(clusterPolygon, Color.RED, null, null));
+        }
+        drawingCommands.add(new DrawMultiPoint(multiPoint, Color.WHITE, null));
+        show(
+                "JTS Visualisation - Convex Hull",
+                drawingCommands.toArray(new DrawingCommand[0])
+        );
+    }
+
+    private static Point[] coordinatesToPoints(int pointCount, List<Coordinate> coordinates, GeometryFactory geometryFactory) {
+        Point[] points = new Point[pointCount];
+        for (int i = 0; i < coordinates.size(); i++) {
+            points[i] = geometryFactory.createPoint(coordinates.get(i));
+        }
+        return points;
+    }
+
+    private record ClusterableCoordinate(Coordinate coordinate) implements Clusterable {
+        @Override
+        public double[] getPoint() {
+            return new double[]{coordinate.x, coordinate.y};
+        }
+    }
+
+
+}
