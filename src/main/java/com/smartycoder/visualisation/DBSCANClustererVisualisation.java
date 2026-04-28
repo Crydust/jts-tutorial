40066104caa3089da67132597e9d75d71fd114f2
Kristof Neirynck <Crydust@users.noreply.github.com>
2026-04-28 12:14:39 +0200

    Kinda works

--- src/main/java/com/smartycoder/visualisation/DBSCANClustererVisualisation.java
+++ src/main/java/com/smartycoder/visualisation/DBSCANClustererVisualisation.java
@@ -6,22 +6,23 @@ import com.smartycoder.ui.DrawingCommand;
 import org.hipparchus.clustering.CentroidCluster;
 import org.hipparchus.clustering.Cluster;
 import org.hipparchus.clustering.Clusterable;
-import org.hipparchus.clustering.DBSCANClusterer;
 import org.hipparchus.clustering.FuzzyKMeansClusterer;
-import org.hipparchus.clustering.distance.EuclideanDistance;
-import org.locationtech.jts.coverage.CoverageUnion;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryCollection;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.MultiPoint;
-import org.locationtech.jts.geom.MultiPolygon;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.Polygon;
 import org.locationtech.jts.geom.impl.CoordinateArraySequence;
+import org.locationtech.jts.triangulate.VoronoiDiagramBuilder;
+import org.locationtech.jts.coverage.CoverageUnion;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.smartycoder.ui.VisualisationUtil.show;
 
@@ -51,24 +52,66 @@ public class DBSCANClustererVisualisation {
             clusterPolygons.add(clusterPolygon);
         }
 
-        // CoverageUnion
-        MultiPolygon union = (MultiPolygon) CoverageUnion.union(clusterPolygons.toArray(new Polygon[0]));
-        clusterPolygons.clear();
-        for (int i = 0; i < union.getNumGeometries(); i++) {
-            Polygon polygon = (Polygon) union.getGeometryN(i);
-            clusterPolygons.add(polygon);
-        }
-
         List<DrawingCommand> drawingCommands = new ArrayList<>();
         for (Polygon clusterPolygon : clusterPolygons) {
-            drawingCommands.add(new DrawPolygon(clusterPolygon, Color.RED, null, null));
+            drawingCommands.add(new DrawPolygon(clusterPolygon, null, Color.GRAY, null));
         }
+
+        // Create Voronoi diagram from ALL points
+        VoronoiDiagramBuilder voronoiBuilder = new VoronoiDiagramBuilder();
+        voronoiBuilder.setSites(coordinates);
+        Geometry voronoiDiagram = voronoiBuilder.getDiagram(geometryFactory);
+
+        // Map each coordinate to its cluster index
+        Map<Coordinate, Integer> coordinateToCluster = new HashMap<>();
+        for (int clusterIdx = 0; clusterIdx < clusters.size(); clusterIdx++) {
+            Cluster<ClusterableCoordinate> cluster = clusters.get(clusterIdx);
+            for (ClusterableCoordinate cc : cluster.getPoints()) {
+                coordinateToCluster.put(cc.coordinate(), clusterIdx);
+            }
+        }
+
+        // Group Voronoi cells by cluster and merge them
+        @SuppressWarnings("unchecked")
+        List<Polygon>[] clusterVoronoiCells = new List[clusters.size()];
+        for (int i = 0; i < clusters.size(); i++) {
+            clusterVoronoiCells[i] = new ArrayList<>();
+        }
+
+        if (voronoiDiagram instanceof GeometryCollection geometryCollection) {
+            for (int i = 0; i < geometryCollection.getNumGeometries(); i++) {
+                Polygon voronoiCell = (Polygon) geometryCollection.getGeometryN(i);
+                // Find which cluster this Voronoi cell belongs to
+                // Find the contained point in coordinates to determine cluster
+                int clusterIdx = 0;
+                for (Coordinate coord : coordinates) {
+                    if (voronoiCell.contains(geometryFactory.createPoint(coord))) {
+                        clusterIdx = coordinateToCluster.getOrDefault(coord, 0);
+                        break;
+                    }
+                }
+
+                clusterVoronoiCells[clusterIdx].add(voronoiCell);
+            }
+        }
+
+        // Merge Voronoi cells for each cluster
+        for (int clusterIdx = 0; clusterIdx < clusters.size(); clusterIdx++) {
+            if (!clusterVoronoiCells[clusterIdx].isEmpty()) {
+                Geometry g = CoverageUnion.union(clusterVoronoiCells[clusterIdx].toArray(new Polygon[0]));
+                if (g instanceof Polygon merged) {
+                    drawingCommands.add(new DrawPolygon(merged, Color.BLUE, null, null));
+                }
+            }
+        }
+
         drawingCommands.add(new DrawMultiPoint(multiPoint, Color.WHITE, null));
         show(
                 "JTS Visualisation - Convex Hull",
                 drawingCommands.toArray(new DrawingCommand[0])
         );
     }
+
 
     private static Point[] coordinatesToPoints(int pointCount, List<Coordinate> coordinates, GeometryFactory geometryFactory) {
         Point[] points = new Point[pointCount];
